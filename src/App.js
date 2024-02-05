ed4cd4567ef47edf2780169a964eea3a06e9331e
SvdNeo <sanjay.d@neoito.com>
2024-02-05 23:55:52 +0530

    created the onAddstudent function and declared usestate

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Create from './components/Create';
 import { useState } from'react';
 
 const App = () => {
-  
+  const [students, setStudents] = useState([]);
+
+  const handleAddStudent = (newStudent) => {
+    setStudents([...students, newStudent]);
+  };
   return (
     <Router>
       <div>
